0de72905cc70f2a43983bbb6a100916ed9f9f1d1
AramCa <AramCagli@gmail.com>
2022-12-09 12:36:12 +0100

    random getaal

--- ifelse/app.js
+++ ifelse/app.js
@@ -2,8 +2,26 @@ class App
 {
         runApplication()
         {
-            let nt = document.getElementById("newstitle");
+            let title = document.getElementById("newstitle")
+            let randomgetal = Math.random();
+            console.log(randomgetal);
 
+            if (randomgetal == 0)
+            {
+                title.style.backgroundColor ="#00FF00";
+            }
+            else if (randomgetal < 0.2)
+            {
+                title.style.backgroundColor ="#FF0000";
+            }
+            else if (randomgetal >= 0.2 && randomgetal< 0.6)
+            {
+                title.style.backgroundColor ="#FFFF00";
+            }
+            else if (randomgetal > 0.6)
+            {
+                title.style.backgroundColor ="#0000FF";
+            }
         }
 }
 
